35c2ebafbdc4ddb251e04066ba5d0a8b50c1bced
Ben Penwell <benpenwell@gmail.com>
2024-07-27 10:43:39 -0700

    Add broken dropdown to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ export interface HeaderProps {
 export const Header = (props: HeaderProps) => {
   const { user } = props;
   const redirectApi: RedirectAPI = new RedirectAPI();
-  //const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const handleLogOutOnClick = () => {
     redirectApi.redirectToPage(PAGE_PATH.LOGOUT);
@@ -28,7 +28,7 @@ export const Header = (props: HeaderProps) => {
   }; 
   
   const handleDropdownToggle = () => {
-    /*setDropdownOpen(!isDropdownOpen);*/
+    setDropdownOpen(prevState => !prevState);
   };
 
   return (
@@ -41,15 +41,15 @@ export const Header = (props: HeaderProps) => {
                 <path
                   d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
                   fill="#FFF"
-                  />
+                />
                 <path
                   d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
                   fill="#555AB9"
-                  />
+                />
                 <path
                   d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
                   fill="#91BAF8"
-                  />
+                />
               </g>
             </svg>
             <h1>Acme</h1>
@@ -57,28 +57,26 @@ export const Header = (props: HeaderProps) => {
         </div>
         <div>
           <Button size="small" onClick={handleDropdownToggle} label="Tools" />
-          {/*isDropdownOpen &&*/ (
-                <nav className="dropdown-menu">
-                  <ul>
-                    {Object.values(KNOWN_TOOL_NAMES).map((toolName) => (
-                      <li key={toolName}>
-                        <a href={`/tools/${toolName}`}>
-                          {toolName}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              )}
+          <nav className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`}>
+            <ul>
+              {Object.values(KNOWN_TOOL_NAMES).map((toolName) => (
+                <li key={toolName}>
+                  <a href={`/tools/${toolName}`}>
+                    {toolName}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
         <div>
           { user ? (
             <Button size="small" onClick={handleLogOutOnClick} label="Log out" />
             ) : (
               <>
-              <Button size="small" onClick={handleLogInOnClick} label="Log in" />
-              <Button primary size="small" onClick={handleSignUpOnClick} label="Sign up" />
-            </>
+                <Button size="small" onClick={handleLogInOnClick} label="Log in" />
+                <Button primary size="small" onClick={handleSignUpOnClick} label="Sign up" />
+              </>
           )}
         </div>
       </div>
