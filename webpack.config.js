1d1ec3bc0638f15e6118d5501932d1feff4ed220
Ben Penwell <benpenwell@gmail.com>
2024-07-26 17:01:58 -0700

    Fix css issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,31 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom'
-  }
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      filename: 'index.js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx']
+    },  
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+              },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    externals: {
+        react: 'react'
+    },
 };
