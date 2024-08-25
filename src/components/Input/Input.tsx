4c375e0a53ea83333f1d7db608aad58c36223153
Ben Penwell <benpenwell@gmail.com>
2024-08-25 11:32:47 -0700

    Implement Smart Input Carats
    - For $ inputs, we automatically shift to the right of the $
    - For % inputs, we automatically shift to the left of the %

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import './Input.css'; // Import the CSS file
 
@@ -9,48 +9,115 @@ const Asterisk = styled.span`
 `;
 
 interface InputProps {
-    label: string;
-    type?: string;
-    options?: string[]; // Add options prop for select input
-    value: any;
-    onChange: (value: any) => void;
-    required?: boolean;
+  id?: string;
+  label: string;
+  type?: string;
+  options?: string[];
+  value: any;
+  onChange: (value: any) => void;
+  required?: boolean;
 }
 
 export const Input = (props: InputProps) => {
-    const { label, type = 'text', options, value, onChange, required } = props;
+  const { id, label, type = 'text', options, value, onChange, required } = props;
 
-    const renderInput = () => {
-        if (type === 'select' && options) {
-            return (
-                <select value={value} onChange={(e) => onChange(e.target.value)} className='input-field'>
-                    {options.map((option) => (
-                        <option key={option} value={option}>
-                            {option}
-                        </option>
-                    ))}
-                </select>
-            );
-        } else {
-            return (
-                <input
-                    required={required}
-                    type={type}
-                    value={value}
-                    onChange={(e) => onChange(e.target.value)}
-                    className='input-field'
-                />
-            );
+  const [displayValue, setDisplayValue] = useState<string>(value === undefined ? '' : value.toString());
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (type === 'currency') {
+      setDisplayValue(displayValue ? formatCurrency(displayValue) : '');
+    } else if (type === 'percent') {
+      setDisplayValue(displayValue ? `${parseFloat(displayValue).toString()}%` : '');
+      adjustCaretPosition('percent');
+    } else {
+      setDisplayValue(displayValue.toString());
+    }
+  }, [value]);
+
+  const formatCurrency = (value: string) => {
+    const numericValue = parseFloat(value.replace(/[^\d.]/g, ''));
+    if (isNaN(numericValue)) return '';
+    return `$${numericValue.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}`;
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    let inputValue = e.target.value.replace('$', '').replace('%', '');
+    let rawValue: string = '';
+
+    if (type === 'currency') {
+      rawValue = inputValue.replace(/[^0-9.]/g, '');
+      const parts = rawValue.split('.');
+      if (parts.length > 2) {
+        rawValue = `${parts[0]}.${parts[1]}`;
+      }
+      setDisplayValue(formatCurrency(rawValue));
+      adjustCaretPosition('currency');
+    } else if (type === 'percent') {
+      rawValue = inputValue.replace(/[^\d.]/g, '');
+      setDisplayValue(rawValue ? `${rawValue}%` : '');
+      adjustCaretPosition('percent');
+    } else {
+      setDisplayValue(inputValue);
+    }
+
+    onChange(rawValue);
+  };
+
+  const adjustCaretPosition = (inputType: string) => {
+    if (inputRef.current) {
+      let caretPosition = inputRef.current.selectionStart || 0;
+
+      if (inputType === 'currency') {
+        if (caretPosition === 0) {
+          inputRef.current.selectionStart = inputRef.current.selectionEnd = 1;
         }
-    };
+      } else if (inputType === 'percent') {
+        if (caretPosition >= inputRef.current.value.length) {
+          inputRef.current.selectionStart = inputRef.current.selectionEnd = inputRef.current.value.length - 1;
+        }
+      }
+    }
+  };
 
-    return (
-        <div className='input-container'>
-            <label className='label'>
-                {label}
-                {required && <Asterisk>*</Asterisk>}
-            </label>
-            {renderInput()}
-        </div>
-    );
+  const handleSmartCaret = (e: React.MouseEvent<HTMLInputElement>) => {
+    adjustCaretPosition(type);
+  };
+
+  const renderInput = () => {
+    if (type === 'select' && options) {
+      return (
+        <select id={id} value={displayValue} onChange={handleInputChange} className='input-field'>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      );
+    } else {
+      return (
+        <input
+          id={id}
+          required={required}
+          type={type}
+          value={displayValue}
+          onChange={handleInputChange}
+          onClick={handleSmartCaret}
+          ref={inputRef}
+          className='input-field'
+        />
+      );
+    }
+  };
+
+  return (
+    <div className='input-container'>
+      <label htmlFor={id} className='label'>
+        {label}
+        {required && <Asterisk>*</Asterisk>}
+      </label>
+      {renderInput()}
+    </div>
+  );
 };
