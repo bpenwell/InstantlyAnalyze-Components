8369b1d8f680ce7dff3ced450903110ab2fa34ab
Ben Penwell <benpenwell@gmail.com>
2024-10-25 00:14:56 -0700

    Update AddressForm.tsx

--- src/components/AddressForm/AddressForm.tsx
+++ src/components/AddressForm/AddressForm.tsx
@@ -1,81 +1,91 @@
-  import React, { useState } from 'react';
-  import { Input } from '../Input/Input';
-  import {
-    BackendAPI,
-    initialRentalCalculatorFormState,
-    PropertyAddress,
-    USState,
-    IRentcastPropertyData,
-  } from '@bpenwell/rei-module';
-  import { Button } from '../Button/Button';
-  import './AddressForm.css';
+import React, { useState, useEffect } from 'react';
+import { Input } from '../Input/Input';
+import {
+  BackendAPI,
+  initialRentalCalculatorFormState,
+  PropertyAddress,
+  USState,
+  IRentcastPropertyData,
+} from '@bpenwell/rei-module';
+import { Button } from '../Button/Button';
+import './AddressForm.css';
+import { LoadingBar } from '../LoadingBar/LoadingBar';
 
-  export type ReturnPropertyData = (addressData: any) => void;
+export type ReturnPropertyData = (addressData: any) => void;
 
-  export const AddressForm = ({ returnResponseData }: { returnResponseData: ReturnPropertyData }) => {
-    const [streetAddress, setStreetAddress] = useState(initialRentalCalculatorFormState.propertyInformation.streetAddress);
-    const [city, setCity] = useState(initialRentalCalculatorFormState.propertyInformation.city);
-    const [state, setState] = useState(initialRentalCalculatorFormState.propertyInformation.state);
-    const [zipCode, setZipCode] = useState(initialRentalCalculatorFormState.propertyInformation.zipCode);
-    const [loading, setLoading] = useState(false);
+export const AddressForm = ({ returnResponseData, triggerAddressSearch }: { returnResponseData: ReturnPropertyData, triggerAddressSearch?: boolean }) => {
+  const [streetAddress, setStreetAddress] = useState(initialRentalCalculatorFormState.propertyInformation.streetAddress);
+  const [city, setCity] = useState(initialRentalCalculatorFormState.propertyInformation.city);
+  const [state, setState] = useState(initialRentalCalculatorFormState.propertyInformation.state);
+  const [zipCode, setZipCode] = useState(initialRentalCalculatorFormState.propertyInformation.zipCode);
+  const [loading, setLoading] = useState(false);
+  const backendAPI = new BackendAPI();
 
-    const handleSubmit = async () => {
-      // Construct the full address object
-      setLoading(true);
-      const address: PropertyAddress = {
-        streetAddress,
-        city,
-        state,
-        zipCode,
-      };
-
-      try {
-        const backendAPI: BackendAPI = new BackendAPI();
-        const rentcastPropertyData: IRentcastPropertyData = await backendAPI.getPropertyInfoByAddress(address);
-        // Pass the data back to the parent component to handle
-        returnResponseData(rentcastPropertyData);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching property data:', error);
-      }
+  const handleSubmit = async () => {
+    setLoading(true);
+    const address: PropertyAddress = {
+      streetAddress,
+      city,
+      state,
+      zipCode,
     };
 
-    return (
-      <div className='addressFormContainer'>
-        <h3>Enter Property Address</h3>
-        <Input
-          id="street"
-          label="Street"
-          value={streetAddress}
-          onChange={(value: string) => setStreetAddress(value)}
-        />
-        <Input
-          id="city"
-          label="City"
-          value={city}
-          onChange={(value: string) => setCity(value)}
-        />
-        <Input
-          id="state"
-          label="State"
-          value={state}
-          options={Object.values(USState)}
-          onChange={(value: USState) => setState(value)}
-        />
-        <Input
-          id="zipCode"
-          label="Zip Code"
-          value={zipCode}
-          onChange={(value: string) => setZipCode(value)}
-        />
-        <Button
-          buttonType={"primary"}
-          size="small"
-          onClick={handleSubmit}
-          style={{padding: "10px 20px", margin: "10px 0px 5px 10px"}}
-          label="Get Property Data"
-          loading={loading}
-        />
-      </div>
-    );
+    try {
+      const rentcastPropertyData: IRentcastPropertyData = await backendAPI.getPropertyInfoByAddress(address);
+      console.log('rentcastPropertyData');
+      console.log(rentcastPropertyData);
+      returnResponseData(rentcastPropertyData);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching property data:', error);
+    }
   };
+
+  useEffect(() => {
+    if (!triggerAddressSearch) { return; }
+    handleSubmit();
+  }, [triggerAddressSearch]);
+
+  return (
+    <div className='addressFormContainer'>
+      <Input
+        id="street"
+        label="Street"
+        value={streetAddress}
+        onChange={(value: string) => setStreetAddress(value)}
+      />
+      <Input
+        id="city"
+        label="City"
+        value={city}
+        onChange={(value: string) => setCity(value)}
+      />
+      <Input
+        id="state"
+        label="State"
+        value={state}
+        options={Object.values(USState)}
+        onChange={(value: USState) => setState(value)}
+      />
+      <Input
+        id="zipCode"
+        label="Zip Code"
+        value={zipCode}
+        onChange={(value: string) => setZipCode(value)}
+      />
+      { loading ? <LoadingBar/> : <></>}
+      {/* triggerSubmit is currently optional to ensure V2 still works */}
+      { triggerAddressSearch !== undefined ? (<></>) : (
+          <Button
+            buttonType={"primary"}
+            size="small"
+            onClick={handleSubmit}
+            style={{padding: "10px 20px", margin: "10px 0px 5px 10px"}}
+            label="Get Property Data"
+            loading={loading}
+          />
+        )
+      }
+    </div>
+  );
+};
